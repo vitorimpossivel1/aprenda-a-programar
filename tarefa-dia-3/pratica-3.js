b72b32b84ee37a835fc4902d864e69192fac3f21
vitor oliveira <vitorimpossivel1@gmail.com>
2022-08-29 23:39:45 -0300

    pratica-3.js

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,14 @@
+// Exibindo numeros impares 
+
+let num1 = 0
+let num2 = 16
+let cont = 1
+
+while ( num1 <= num2)
+{
+       if (num1 % 2 == 1) 
+            {
+             console.log(num1)
+            }
+        num1++
+}
