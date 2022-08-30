416a71e66f6205029e3ec13b8768f9b0ea7ada69
vitor oliveira <vitorimpossivel1@gmail.com>
2022-08-29 23:11:35 -0300

    pratica-2.js

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,14 @@
+// treinando tabuada 
+
+let multiplicador = 1
+let n = 10
+let i = 1
+
+while (i <= n) {
+    let resultado = multiplicador  * i
+    let textoDeExibicao = `${multiplicador} X ${i} = ${resultado}`
+console.log(textoDeExibicao)
+i++
+}
+
+
